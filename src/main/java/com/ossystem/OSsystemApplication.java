af484f290ad49f9785cf8fd2669d68a3383cde72
okrabot <jungjihoonkr97@gmail.com>
2021-06-02 23:20:43 +0700

    init project

---
+++ src/main/java/com/ossystem/OSsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OSsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OSsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ossystem/OSsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OSsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
